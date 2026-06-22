78bce4fc4cc8af2078071c42a9796f47296aac05
npm1nstall <theclown015@gmail.com>
2026-06-22 18:22:44 +0700

    Add database configuration example file

---
+++ database.example.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'   => '',
+    'hostname' => 'localhost',
+    'username' => 'USERNAME_KAMU',
+    'password' => 'PASSWORD_KAMU',   
+    'database' => 'NAMA_DATABASE', 
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE,
+    'session_variables' => array('time_zone' => '+07:00')
+);
